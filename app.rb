7ef8c6c69f86412d31d9673b537e8cd8841889b6
kleer <kleer@kleer-csd-box>
2017-07-26 11:44:27 -0500

    refactor

--- app.rb
+++ app.rb
@@ -1,23 +1,27 @@
 require 'sinatra'
 require './config'
-require './lib/Aleatorio'
+require './lib/Pifia2'
 
 get '/' do
+	session['pifia2'] = Pifia2.new
 	session['intentos']=10
 	session['parcial']="#{session['intentos']} intentos restantes"
-	
+	session['numOculto']= session['pifia2'].generarNumero 1
 	erb(:index)
 end
 
 get '/numOc' do
-	session['numOculto']=new Aleatorio.new.generarNumero 1
 	
 	erb(:'pages/numAleatorio')
 end
 
 post '/go' do
+	num = params['num'];
 	session['intentos'] -= 1
-	session['parcial'] = "#{params['num']}: 1P #{session['intentos']} intentos restantes"
+	
+	picas = session['pifia2'].validarPica session['numOculto'], num
+	
+	session['parcial'] = "#{num}: #{picas}P #{session['intentos']} intentos restantes"
 	
 	if(session['intentos'] == 0)
 		erb(:'pages/lose')
